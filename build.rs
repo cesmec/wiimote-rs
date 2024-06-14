d40900b095c9eef9bf94f859f4182a5810d3f751
cesmec <cesmec@users.noreply.github.com>
2024-06-14 13:05:08 +0200

    Remove bindgen build dependency on windows

--- build.rs
+++ build.rs
@@ -1,21 +1,22 @@
-use std::path::PathBuf;
-
+#[cfg(target_os = "linux")]
 fn main() {
-    if cfg!(target_os = "linux") {
-        const HEADER_FILE: &str = "src/native/linux/bluetooth_linux.h";
-        println!("cargo:rerun-if-changed={HEADER_FILE}");
+    const HEADER_FILE: &str = "src/native/linux/bluetooth_linux.h";
+    println!("cargo:rerun-if-changed={HEADER_FILE}");
 
-        let bindings = bindgen::Builder::default()
-            .header(HEADER_FILE)
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-            .generate()
-            .expect("Failed to generate bindings for libbluetooth");
+    let bindings = bindgen::Builder::default()
+        .header(HEADER_FILE)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Failed to generate bindings for libbluetooth");
 
-        let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-        bindings
-            .write_to_file(out_dir.join("bindings.rs"))
-            .expect("Failed to write bindings for libbluetooth");
+    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_dir.join("bindings.rs"))
+        .expect("Failed to write bindings for libbluetooth");
 
-        println!("cargo:rustc-link-lib=bluetooth");
-    }
+    println!("cargo:rustc-link-lib=bluetooth");
+}
+
+#[cfg(target_os = "windows")]
+fn main() {
 }
